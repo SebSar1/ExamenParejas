a23abaacacc703e50b13c9677f0828c1055a682c
MrGOnorrea <jaangulo04@gmail.com>
2024-02-29 14:12:18 -0500

    SE añadio el boton para el inicio

--- ExamenAntBot/src/App.java
+++ ExamenAntBot/src/App.java
@@ -3,21 +3,29 @@ import java.util.ArrayList;
 
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
 
 import BusinessLogic.BL.HormigaBL;
-import InteligenciaArtificial.Rusia;
 import DataAccess.DTO.HormigaDTO;
 import UserInterface.Form.MainForm;
 import UserInterface.Form.SplashScreenForm;
 
 public class App {
     public static void main(String[] args) throws InterruptedException {
-
+        mostrarMensaje();
         iniciarSistema();
     }
-
+    
+    public static void mostrarMensaje() {
+        String mensaje = "Este programa se dividira en dos partes,la primera se mostrara en la terminal y la segunda se usara un interfaz";
+        String titulo = "!Bienvenido PachaLover!";
+        int opcion = JOptionPane.showConfirmDialog(null, mensaje, titulo, JOptionPane.DEFAULT_OPTION, JOptionPane.INFORMATION_MESSAGE);
+        if (opcion == JOptionPane.OK_OPTION || opcion == JOptionPane.CLOSED_OPTION) {
+            return; 
+        }
+    }
     public static void iniciarSistema() throws InterruptedException {
         SplashScreenForm.show();
         // Rusia oRusia = new Rusia();
